c086a7a928d8e128e820f3f290b89a2f53ab6923
Baraa Al-Masri <pub@mbaraa.com>
2024-02-21 13:21:33 +0300

    Add a cli tool that can be switched with a flag

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"embed"
 	"encoding/json"
+	"errors"
+	"flag"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"readmetmpl/log"
 	"readmetmpl/templates"
 )
@@ -13,16 +16,89 @@ import (
 var (
 	//go:embed resources/*
 	res embed.FS
+
+	// Version contains the application's version number,
+	// It's set by ldflags on build time.
+	Version = ""
 )
 
 func main() {
+	version := flag.Bool("version", false, "display version")
+	help := flag.Bool("help", false, "display this screen")
+	serverPort := flag.String("port", "", "web server's hosting port")
+	filePath := flag.String("file", "", "path to the JSON file")
+	flag.Parse()
+
+	var err error
+
+	switch {
+	case *version:
+		displayVersion()
+	case len(*serverPort) != 0:
+		err = startWebServer()
+	case len(*filePath) != 0:
+		err = generateReadmeFromJson(*filePath, func(tmpl templates.ReadmeProps) (io.Reader, error) {
+			readme := templates.NewReadme().Render(tmpl)
+			return readme, nil
+		})
+	case *help:
+		fallthrough
+	default:
+		flag.PrintDefaults()
+	}
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func displayVersion() {
+	if Version == "" {
+		Version = "(built from source)"
+	}
+	fmt.Printf("readmetmpl %s\n", Version)
+	os.Exit(0)
+}
+
+func generateReadmeFromJson[T any](filePath string, renderFunc func(T) (io.Reader, error)) error {
+	if filePath == "" {
+		return errors.New("empty file path")
+	}
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return err
+	}
+	var jsonData T
+	err = json.Unmarshal(data, &jsonData)
+	if err != nil {
+		return err
+	}
+	readme, err := renderFunc(jsonData)
+	if err != nil {
+		return err
+	}
+	file, err := os.Create("Readme.md")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	_, err = io.Copy(file, readme)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func startWebServer() error {
 	http.HandleFunc("/", handleHomePage)
 
 	http.HandleFunc("/api/generate-readme", handleGenerateReadme)
 
 	http.Handle("/resources/", http.FileServer(http.FS(res)))
 	log.Infof("dashboard's server started at port %s\n", "8081")
-	log.Fatalln(log.ErrorLevel, http.ListenAndServe(":"+"8081", nil))
+	return http.ListenAndServe(":"+"8081", nil)
 }
 
 func handleGenerateReadme(w http.ResponseWriter, r *http.Request) {
